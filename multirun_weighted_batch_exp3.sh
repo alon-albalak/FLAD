8c411a0c5814411a301189b642bbc3a4e8951de8
Alon Albalak <alon_albalak@ucsb.edu>
2022-12-22 16:02:41 -0800

    fix folder name

--- multirun_weighted_batch_exp3.sh
+++ multirun_weighted_batch_exp3.sh
@@ -7,7 +7,7 @@ WEIGHT_INIT_SAMPLES=$5
 SEEDS=( 42 100 222 3456 5876 )
 
 for SEED in ${SEEDS[@]}; do
-    OUTPUT_DIR="outputs/train_logs/$SEED/$MODEL/${AUX_DATASET}/${TARGET_DATASET}/${WEIGHT_INIT_SAMPLES}"
+    OUTPUT_DIR="outputs/train_logs/exp3/$SEED/$MODEL/${AUX_DATASET}/${TARGET_DATASET}/${WEIGHT_INIT_SAMPLES}"
     mkdir -p $OUTPUT_DIR
 
     CUDA_VISIBLE_DEVICES=$GPU python src/multirun_train_weighted_batch_exp3.py \
